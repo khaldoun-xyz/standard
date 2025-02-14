16f2a498ffdf84c2156b9e0c2dbd1c21670ce881
s0288 <alex.gansmann@hey.com>
2025-02-14 18:38:55 +0100

    setup: add install script & readme

---
+++ setup/README.md
@@ -0,0 +1,41 @@
+# Initial set up to get going quickly
+This repo contains an installer script that sets up Khaldoun's 
+recommended configurations and development tools.
+
+## Install 
+Execute in this sequence:
+- clone this repo and navigate to the folder `setup`
+- run `. khaldoun-setup.sh`
+
+## .bashrc
+- add this to the bottom of your `~/.bashrc`:
+
+```bash
+# --------------------------- manually add to .bashrc --------------------------------
+# show git branch in Terminal
+function parse_git_branch() {
+  git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+RED="\[\033[01;31m\]"
+YELLOW="\[\033[01;33m\]"
+GREEN="\[\033[01;32m\]"
+BLUE="\[\033[01;34m\]"
+NO_COLOR="\[\033[00m\]"
+PS1="$GREEN\u$NO_COLOR:$BLUE\w$YELLOW\$(parse_git_branch)$NO_COLOR\$ "
+
+# add venv aliases
+alias lugha='cd ~/programming/lugha && . ~/virtualenvs/lugha/bin/activate'
+alias sisu='cd ~/programming/sisu && . ~/virtualenvs/sisu/bin/activate'
+alias terminal_llm='cd ~/programming/terminal_llm/src && . ~/virtualenvs/terminal_llm/bin/activate && python chat.py'
+alias khaldoun='cd ~/programming/khaldoun && . ~/virtualenvs/khaldoun/bin/activate'
+
+# add neovim alias
+alias n='~/programming/nvim-linux-x86_64.appimage'
+```
+
+## Troubleshooting
+- if your wifi is not available, follow these steps: 
+  https://askubuntu.com/questions/55868/installing-broadcom-wireless-drivers
+- if your webcam is not available, follow the Debian steps: 
+  https://github.com/patjak/facetimehd/wiki/Installation#get-started-on-debian
+
---
+++ setup/khaldoun-setup.sh
@@ -0,0 +1,97 @@
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt-get install snapd -y # snap store
+sudo apt install python3-pip -y
+sudo apt install postgresql -y
+sudo apt install tmux -y
+sudo apt install docker-compose -y
+sudo apt install docker.io -y
+sudo usermod -aG docker $USER # run this to not require sudo for docker commands
+newgrp docker
+sudo apt install alacritty -y
+sudo snap install zellij --classic
+sudo apt install ripgrep -y # for finding files based on words
+sudo apt-get install gnome-screenshot -y
+sudo apt-get install gthumb -y
+sudo apt install htop -y
+sudo apt-get install cheese -y
+sudo pip3 install virtualenv
+sudo apt reinstall --purge bluez gnome-bluetooth # to preempt headphone issues
+
+mkdir ~/virtualenvs
+mkdir ~/programming/
+cd ~/programming/
+
+git config --global credential.helper store
+
+## khaldoun
+# lugha
+git clone https://github.com/khaldoun-xyz/lugha.git
+virtualenv ~/virtualenvs/lugha
+. ~/virtualenvs/lugha/bin/activate
+pip install -r lugha/requirements.txt
+# sisu
+git clone https://github.com/khaldoun-xyz/sisu.git
+virtualenv ~/virtualenvs/sisu
+. ~/virtualenvs/sisu/bin/activate
+pip install -r sisu/requirements.txt
+# terminal_llm
+git clone https://github.com/khaldoun-xyz/terminal_llm.git
+virtualenv ~/virtualenvs/terminal_llm
+. ~/virtualenvs/terminal_llm/bin/activate
+pip install -r terminal_llm/requirements.txt
+# renard
+git clone https://github.com/khaldoun-xyz/renard.git
+virtualenv ~/virtualenvs/renard
+. ~/virtualenvs/renard/bin/activate
+pip install -r renard/requirements.txt
+# core_skills
+git clone https://github.com/khaldoun-xyz/core_skills.git
+# khaldoun hp
+git clone https://github.com/khaldoun-xyz/khaldoun.git
+virtualenv ~/virtualenvs/khaldoun
+. ~/virtualenvs/khaldoun/bin/activate
+pip install -r khaldoun/requirements.txt
+deactivate
+
+# neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+# lazygit for neovim
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm -rf lazygit.tar.gz
+# lazydocker
+LAZYDOCKER_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+mkdir lazydocker-temp
+tar xf lazydocker.tar.gz -C lazydocker-temp
+sudo mv lazydocker-temp/lazydocker /usr/local/bin
+rm -rf lazydocker.tar.gz lazydocker-temp
+# install fonts for alacritty to show nvim icons properly
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip
+unzip FiraCode.zip -d ~/.local/share/fonts
+rm FiraCode.zip
+fc-cache -fv
+mkdir ~/.config/alacritty/
+cat <<EOF >>~/.config/alacritty/alacritty.yml
+font:
+  normal:
+    family: "FiraCode Nerd Font"
+    size: 12.0
+  bold:
+    family: "FiraCode Nerd Font"
+  italic:
+    family: "FiraCode Nerd Font"
+env:
+  LANG: en_US.UTF-8
+EOF
+
+# install brave
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install brave-browser -y
